1320501a0a8efaaf60f80f206e222af0ebb5d8b3
Kevin Scott <kevin@scottlabsllc.com>
2016-11-27 22:23:59 -0500

    Add latest

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const NODE_ENV = process.env.NODE_ENV || 'production';
 
 export const HOST = process.env.HOST || 'localhost';
 export const PORT = process.env.PORT || 3000;
