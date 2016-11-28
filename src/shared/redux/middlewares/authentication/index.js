7ad23e72aee0ad058b125fa1a8e7bd401ce136e7
Kevin Scott <kevin@scottlabsllc.com>
2016-11-27 20:25:27 -0500

    Remove login

--- src/shared/redux/middlewares/authentication/index.js
+++
@@ -1,5 +0,0 @@
-import middleware from './middleware';
-export * as types from './types';
-export * as actions from './actions';
-
-export default middleware;
--- src/shared/redux/middlewares/authentication/types.js
+++
@@ -1,4 +0,0 @@
-const KEY = 'middleware/@Authentication/';
-
-export const SAVE_TOKEN = `${KEY}SAVE_TOKEN`;
-export const UNAUTHENTICATE = `${KEY}UNAUTHENTICATE`;
